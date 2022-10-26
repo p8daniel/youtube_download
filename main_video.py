240b912361834a7fd8201aac0923c43690339a5a
Daniel Pelati <daniel.pelati@metronlab.com>
2022-10-26 21:38:10 +0200

    feat: ask confirmation for destination folder

--- main_video.py
+++ main_video.py
@@ -1,11 +1,9 @@
 from download_yt import execute_download
-from mylogger import logger
 from utils import DefaultFolderType, get_destination_directory
 
 
 def main():
     destination_folder = get_destination_directory(default_folder=DefaultFolderType.VIDEO)
-    logger.info("Destination folder: %s", destination_folder)
     execute_download(only_audio=False, destination_folder=destination_folder)
 
 
--- utils.py
+++ utils.py
@@ -7,15 +7,27 @@ class DefaultFolderType(str, Enum):
     MUSIC = "Music"
 
 
+def get_console_confirmation(request_message: str) -> bool:
+    """Get confirmation from the user"""
+    while True:
+        answer = input(request_message + " [Y/n] (default: y) ")
+        if answer.lower() in ["", "y", "yes"]:
+            return True
+        elif answer.lower() in ["n", "no"]:
+            return False
+        else:
+            print("Je n'ai pas compris")
+
+
 def get_destination_directory(default_folder: DefaultFolderType) -> Path:
-    music_path_folders = [
-        Path.home() / default_folder.value,
-        # Path("%HOMEPATH%/Videos"),
-        # Path("%UserProfile%/Videos"),
-    ]
-    for path in music_path_folders:
-        if path.exists():
-            return path
+    default_folder_path = Path.home() / default_folder.value
+    if default_folder_path.exists():
+        confirmation_msg = (
+            f"Le fichier sera téléchargé dans: '{default_folder_path}' "
+            "Continuer avec cet emplacement?"
+        )
+        if get_console_confirmation(request_message=confirmation_msg):
+            return default_folder_path
     import easygui as easygui
 
-    return Path(easygui.diropenbox())
+    return Path(easygui.diropenbox(title="Dossier de destination"))
